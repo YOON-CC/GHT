e747d6c5e9b2fcba932fad19b730fa666c7a0711
YOON-CC <whdbscks77@gmail.com>
2025-11-08 20:06:20 +0900

    d1

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 
 const practiceIdeas: PracticeIdea[] = [
   {
-    title: 'TEXT COMPONENT2222',
+    title: 'TEXT COMPONENT2222 ###dev dev dev',
     description: '사용자 이름을 입력받아 화면에 표시하는 컴포넌트를 추가해보세요.',
     difficulty: 'easy'
   },
