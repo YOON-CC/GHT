612a304378d059c2efa4875fa4e5c5bda787fa96
YOON-CC <whdbscks77@gmail.com>
2025-11-08 22:34:41 +0900

    fffff

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 
 const practiceIdeas: PracticeIdea[] = [
   {
-    title: 'TEXT COMPONENDEV',
+    title: 'TEXT COMPONENfix',
     description: '사용자 이름을 입력받아 화면에 표시하는 컴포넌트를 추가해보세요.',
     difficulty: 'easy'
   },
@@ -22,7 +22,7 @@ const practiceIdeas: PracticeIdea[] = [
 ];
 
 // dev 입니다.
-
+// ㅎㅇㅎㅇ
 // fixfix
 // fixfix22
 function App() {
